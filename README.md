1265de49ca877c2940e6bd1efa5145367ff62565
Julian Grey <julianrgrey@gmail.com>
2025-06-30 16:31:14 +0100

    Initial commit, add simple testing data

---
+++ README.md
@@ -0,0 +1,5 @@
+# To Do App Backend
+
+The backend of a small To Do App project, acting as a bit of a playground for me to expand my knowledge into backend, ultimately growing as a full-stack developer.
+
+This contains the APIs which will control the functionality of the frontend, separating the responsibility of the two systems. This will eventually be tied to a database to hold all the information regarding any added to dos, enabling the ability to view, add, edit and delete entries.
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const allowedOrigins = ['http://localhost:5173'];
+
+app.use(express.json());
+app.use(cors({
+  origin: allowedOrigins,
+  optionsSuccessStatus: 200,
+  maxAge: 10,
+}));
+
+app.get('/api/todos', (req, res) => {
+  const todos = [
+    {
+      category: 'Event',
+      description: 'To Do Description',
+      title: 'To Do Title',
+    },
+    {
+      title: 'To Do Title 2',
+    },
+    {
+      description: 'To Do Description 3',
+      title: 'To Do Title 3',
+    },
+  ];
+
+  res.json(todos);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
