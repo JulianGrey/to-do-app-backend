2878eda7a6e0fe52f130f4978794b746d3e846f9
Julian Grey <julianrgrey@gmail.com>
2025-07-08 13:24:48 +0100

    Implement add todo API

--- server.js
+++ server.js
@@ -5,6 +5,21 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const allowedOrigins = ['http://localhost:5173'];
 
+const todos = [
+  {
+    description: 'To Do Description',
+    title: 'To Do Title',
+  },
+  {
+    description: '',
+    title: 'To Do Title 2',
+  },
+  {
+    description: 'To Do Description 3',
+    title: 'To Do Title 3',
+  },
+];
+
 app.use(express.json());
 app.use(cors({
   origin: allowedOrigins,
@@ -13,22 +28,21 @@ app.use(cors({
 }));
 
 app.get('/api/todos', (req, res) => {
-  const todos = [
-    {
-      description: 'To Do Description',
-      title: 'To Do Title',
-    },
-    {
-      description: '',
-      title: 'To Do Title 2',
-    },
-    {
-      description: 'To Do Description 3',
-      title: 'To Do Title 3',
-    },
-  ];
 
   res.json(todos);
+});
+
+app.post('/api/todos/add', (req, res) => {
+  const { description, title } = req.body;
+
+  if (!title) {
+    return res.status(400).json({ error: 'Title is required' });
+  }
+
+  const newTodo = { description: description || '', title };
+  todos.push(newTodo);
+
+  res.status(201).json(newTodo);
 });
 
 app.listen(PORT, () => {
